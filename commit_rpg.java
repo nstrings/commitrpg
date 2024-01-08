7350a38a6d50c0c7813e229042fdc03d8ed886d1
nstrings <124907735+nstrings@users.noreply.github.com>
2024-01-08 15:50:08 +0000

    Initial commit

---
+++ commit_rpg.java
@@ -0,0 +1,11 @@
+public class commit_rpg {
+    public static void main(String[] args){
+        while(true){
+            game_loop();
+
+        }
+    }
+    public static game_loop(){
+
+    }
+}
